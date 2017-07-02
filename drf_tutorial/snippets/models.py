509c2265dfa7c7281aef7a08707081ce22d89cd9
Yunsik Park <space214@gmail.com>
2017-07-02 17:03:18 +0900

    모델에 속성추가

--- drf_tutorial/snippets/models.py
+++ drf_tutorial/snippets/models.py
@@ -1,5 +1,8 @@
 from django.db import models
+from pygments import highlight
+from pygments.formatters.html import HtmlFormatter
 from pygments.lexers import get_all_lexers
+from pygments.lexers import get_lexer_by_name
 from pygments.styles import get_all_styles
 
 LEXERS = [item for item in get_all_lexers() if item[1]]
@@ -8,6 +11,8 @@ STYLE_CHOICES = sorted((item, item) for item in get_all_styles())
 
 
 class Snippet(models.Model):
+    owner = models.ForeignKey('auth.User', related_name='snippets')
+    highlighted = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=100, blank=True, default='')
     code = models.TextField()
@@ -17,3 +22,15 @@ class Snippet(models.Model):
 
     class Meta:
         ordering = ('created',)
+
+
+def save(self, *args, **kwargs):
+    """
+    `pygments` 라이브러리를 사용하여 하이라이트된 코드를 만든다
+    """
+    lexer = get_lexer_by_name(self.language)
+    linenos = self.linenos and 'table' or False
+    options = self.title and {'title': self.title} or {}
+    formatter = HtmlFormatter(style=self.style, linenos=linenos, full=True, **options)
+    self.highlighted = highlight(self.code, lexer, formatter)
+    super(Snippet, self).save(*args, **kwargs)
