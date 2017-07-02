89f35a96cd5b72f51b2e093d935c96a22d4c1460
Yunsik Park <space214@gmail.com>
2017-07-02 18:31:13 +0900

    update Serializers, 뷰에 요청권한추가,API로그인 추가

--- drf_tutorial/snippets/urls.py
+++ drf_tutorial/snippets/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from rest_framework.urlpatterns import format_suffix_patterns
 
 from . import views
@@ -7,7 +7,11 @@ urlpatterns = [
     url(r'^snippets/$', views.SnippetList.as_view()),
     url(r'^snippets/(?P<pk>\d+)/$', views.SnippetDetail.as_view()),
     url(r'^users/$', views.UserList.as_view()),
-    url(r'^users/(?P<pk>\d+/$', views.UserDetail.as_view()),
+    url(r'^users/(?P<pk>\d+)/$', views.UserDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
+
+urlpatterns += [
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+]
